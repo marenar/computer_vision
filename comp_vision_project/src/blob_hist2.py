2c0da603fdbbcee23bb8e30bb16074e08206c6af
marenar <marena.richardson@students.olin.edu>
2014-11-09 12:40:23 -0500

    cleanup

--- comp_vision_project/src/blob_hist2.py
+++ comp_vision_project/src/blob_hist2.py
@@ -17,17 +17,22 @@ class image_converter:
 
   def __init__(self):
     self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    self.image_sub = rospy.Subscriber("/camera/image_raw", Image)
+    self.laser_sub = rospy.Subscriber("/scan", LaserScan)
+    ts = message_filters.TimeSynchronizer([self.image_sub, self.laser_sub], 10)
+    ts.registerCallback(self.callback)
 
     cv2.namedWindow("Image window", 1)
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/camera/image_raw",Image,self.callback)
 
-  def make_color_range(self, hsvColor):
+  def make_color_range(self, rgbColor):
 
     rangeval = 5
 
-    low = [hsvColor[0] - rangeval, hsvColor[1] - rangeval, hsvColor[2] - rangeval] 
-    high = [hsvColor[0] + rangeval, hsvColor[1] + rangeval, hsvColor[2] + rangeval]
+    low = [rgbColor[0] - rangeval, rgbColor[1] - rangeval, rgbColor[2] - rangeval] 
+    high = [rgbColor[0] + rangeval, rgbColor[1] + rangeval, rgbColor[2] + rangeval]
 
     for i in low:
       if i < 0:
@@ -39,17 +44,16 @@ class image_converter:
 
     return [low, high]
 
-  def callback(self,data):
+  def callback(self, image_data, laser_scan):
 
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      cv_image = self.bridge.imgmsg_to_cv2(image_data, "bgr8")
     except CvBridgeError, e:
       print e
 
     cv_image = cv2.blur(cv_image,(3,3))
     image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
     image2 = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
     # reshape the image to be a list of pixels
     image = image.reshape((image.shape[0] * image.shape[1], 3))
@@ -58,44 +62,17 @@ class image_converter:
     clt = KMeans(n_clusters = 3)
     clt.fit(image)
 
-    # build a histogram of clusters and then create a figure
-    # representing the number of pixels labeled to each color
+    # build a histogram of clusters
     hist = self.centroid_histogram(clt)
-    print hist
-    print clt.cluster_centers_
-    #bar = self.plot_colors(hist, clt.cluster_centers_)
-
-    # # show our color bart
-    # plt.figure()
-    # plt.axis("off")
-    # plt.imshow(bar)
-    # plt.show()
-    low = np.array([0,0,0])
-    high = np.array([0,0,0])
-    target = np.array([0,0,0])
 
     percents = list(hist)
     target = clt.cluster_centers_[percents.index(min(percents))].astype("uint8")
 
-    #hsvColor = cv2.cvtColor(integerColor, cv2.COLOR_RGB2HSV)
     [low, high] = self.make_color_range(target)
-    print "low", low
-    print "high", high
 
-    # #opencv HSV range = [180, 255,255]
-    # #target color = 30, 255, 255
-    # #Convert to hsv and find range of colors
-    #print type(target)
-    #hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-    #hsvTarget = cv2.cvtColor(target, cv2.COLOR_RGB2HSV)
-    #thresh = cv2.inRange(image,low, high)
-    #thresh = cv2.inRange(image, np.array((low[0], low[1], low[2])), np.array((high[0], high[1], high[2])))
-    #print type(image), len(image), image[0]
-    #print type(np.array([150,150,150], dtype=np.uint8)), len(np.array([150, 150, 150], dtype=np.uint8))
-    #thresh = cv2.inRange(image2, np.array([150, 150, 150], dtype=np.uint8), np.array([255, 255, 255], dtype=np.uint8))
     thresh = cv2.inRange(image2, np.array(low), np.array(high))
         
-    # #Find contours in the threshold image
+    #Find contours in the threshold image
     contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
     #Finding contour with maximum area and store it as best_cnt
@@ -124,7 +101,6 @@ class image_converter:
     cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
 
-
   def centroid_histogram(self, clt):
     # grab the number of different clusters and create a histogram
     # based on the number of pixels assigned to each cluster
@@ -137,24 +113,6 @@ class image_converter:
 
     # return the histogram
     return hist
-
-  def plot_colors(self, hist, centroids):
-    # initialize the bar chart representing the relative frequency
-    # of each of the colors
-    bar = np.zeros((50, 300, 3), dtype = "uint8")
-    startX = 0
-
-    # loop over the percentage of each cluster and the color of
-    # each cluster
-    for (percent, color) in zip(hist, centroids):
-      print percent, color
-      # plot the relative percentage of each cluster
-      endX = startX + (percent * 300)
-      cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-        color.astype("uint8").tolist(), -1)
-      startX = endX
-    # return the bar chart
-    return bar
 
 
 def main(args):
